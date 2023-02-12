d3d6f709ff8f06ee924962bce7a2dbea6e44acf9
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-12 22:43:16 +0300

    Done with assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+require 'pry'
 
+class Book
+attr_accessor :title
+attr_accessor :author
+attr_accessor :page_count
+attr_accessor :genre
+
+
+    def initialize(title)
+        @title= title        
+    end
+
+    def turn_page        
+        puts "Flipping the page...wow, you read fast!"
+        end
+    end
+
+    book1 = Book.new('And Then There Were None')
+
+    book1.page_count=  272
+    atomic = Book.new("habit")
+book1.author ="John Doe"
+book1.genre = "mystery"
+book1.turn_page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+attr_accessor :brand
+attr_accessor :color
+attr_accessor :size
+attr_accessor :material
+attr_accessor :condition
+
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+ shoe1= Shoe.new("Adidas")
+shoe1.color = "white"
+shoe1.size = 42
+shoe1.material = "leather"
+shoe1.condition = "old"
+
+
+        
